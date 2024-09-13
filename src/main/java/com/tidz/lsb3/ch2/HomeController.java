0110d6a872ffbfddbbb762e2703f4052b2019360
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-09-13 10:35:50 -0300

    use records Model and stuff

--- src/main/java/com/tidz/lsb3/ch2/HomeController.java
+++ src/main/java/com/tidz/lsb3/ch2/HomeController.java
@@ -1,13 +1,22 @@
 package com.tidz.lsb3.ch2;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
+	record Video(String name) {
+	}
+
+	List<Video> videos = List.of(new Video("VHS1"), new Video("Try not to Sleep"), new Video("The Dude!"));
+
 	@GetMapping("/")
-	public String index() {
+	public String index(Model model) {
+		model.addAttribute("videos", videos);
 		return "index";
 	}
 
